7b9b85a69422ab938ee25025043fbab9708af376
sherlockof96 <kavishdoshi28@gmail.com>
2023-05-29 22:14:44 -0700

    admin

---
+++ wp-admin/site-editor.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=http://mavcons.github.io/Website/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2FFirstWebsite%2Fwp-admin%2Fsite-editor.php%3Fsimply_static_page%3D17117&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "http://mavcons.github.io/Website/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2FFirstWebsite%2Fwp-admin%2Fsite-editor.php%3Fsimply_static_page%3D17117&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="http://mavcons.github.io/Website/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2FFirstWebsite%2Fwp-admin%2Fsite-editor.php%3Fsimply_static_page%3D17117&reauth=1">http://mavcons.github.io/Website/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2FFirstWebsite%2Fwp-admin%2Fsite-editor.php%3Fsimply_static_page%3D17117&reauth=1</a></p>
+	</body>
+</html>
